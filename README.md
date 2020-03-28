d08522e77c17b96c523ecde1345d352fcb1e527b
felix <felixyoungzz@163.com>
2020-03-28 20:55:34 +0800

    chore(*): init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-sortable-list
 A repository to build a sortable list with React.js
+
+### Quick Start
+```sh
+# start dev server
+yarn dev
+
+# build production
+yarn build
+```
---
+++ src/pages/app/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './style.scss';
+
+export const App = () => {
+  return (
+    <div className="container">
+      Hello Sortable List!
+    </div>
+  )
+}
